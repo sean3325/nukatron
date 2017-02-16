e399b543314e2073e01e5e70cffcd7c9d818e1e0
Sean Wolfe <sean@zivtech.com>
2017-02-15 22:37:12 -0500

    add bootstrap and data

--- js/main.js
+++ js/main.js
@@ -1,98 +1,32 @@
 var myContainer = document.getElementById("my-container");
+var myChart = document.getElementById("chartDiv");
 
 var ourRequest = new XMLHttpRequest();
 ourRequest.open('GET', 'https://cdn.rawgit.com/sean3325/nukatron/6c9fcf0d/history.json');
 ourRequest.onload = function() {
-  var ourData = JSON.parse(ourRequest.responseText);
-  renderHTML(ourData);
+  var myData = JSON.parse(ourRequest.responseText);
+  renderHTML(myData);
 };
 ourRequest.send();
 
 function renderHTML(data) {
   var htmlString = "";
+  var chartData = {
+    "type":"line",
+    "series": data
+  };
 
   for (i = 0; i < data.length; i++) {
-    htmlString += "<div>" + "<h2>" + data[i].name + "</h2>" + "<p>" + data[i].status + "</p>" + "<p>" + data[i].message + "</p>" + "</div>";
+    htmlString += '<div class="alert">' + "<h2>" + data[i].name + "</h2>" + "<p>" + data[i].status + "</p>" + "<p>" + data[i].message + "</p>" + "</div>";
   }
 
   myContainer.insertAdjacentHTML('beforeend', htmlString);
 
+  zingchart.render({
+    id:'chartDiv',
+    data:chartData,
+    height:400,
+    width:600
+  });
+
 }
-
-/*
-const CHART = document.getElementById("LineChart");
-let LineChart = new Chart(CHART, {
-  type: 'line',
-  data: {
-    labels: ["January", "February", "March", "April", "May", "June", "July"],
-    datasets: [
-      {
-        label: "My First dataset",
-        fill: false,
-        lineTension: 0.1,
-        backgroundColor: "rgba(75,192,192,0.4)",
-        borderColor: "rgba(75,192,192,1)",
-        borderCapStyle: 'butt',
-        borderDash: [],
-        borderDashOffset: 0.0,
-        borderJoinStyle: 'miter',
-        pointBorderColor: "rgba(75,192,192,1)",
-        pointBackgroundColor: "#fff",
-        pointBorderWidth: 1,
-        pointHoverRadius: 5,
-        pointHoverBackgroundColor: "rgba(75,192,192,1)",
-        pointHoverBorderColor: "rgba(220,220,220,1)",
-        pointHoverBorderWidth: 2,
-        pointRadius: 1,
-        pointHitRadius: 10,
-        data: [65, 59, 80, 81, 56, 55, 40, 50],
-        spanGaps: false,
-      },
-      {
-        label: "My Second dataset",
-        fill: true,
-        backgroundColor: "rgba(75,75,192,0.4)",
-        borderColor: "rgba(75,72,192,1)",
-        borderCapStyle: 'butt',
-        borderDash: [],
-        borderDashOffset: 0.0,
-        borderJoinStyle: 'miter',
-        pointBorderColor: "rgba(75,72,192,1)",
-        pointBackgroundColor: "#fff",
-        pointBorderWidth: 1,
-        pointHoverRadius: 5,
-        pointHoverBackgroundColor: "rgba(75,72,192,1)",
-        pointHoverBorderColor: "rgba(220,220,220,1)",
-        pointHoverBorderWidth: 2,
-        pointRadius: 1,
-        pointHitRadius: 10,
-        data: [100, 20, 60, 81, 80, 55, 90],
-        spanGaps: false,
-      },
-    ]
-  },
-  options: {
-    scales: {
-      yAxes: [{
-        ticks: {
-          beginAtZero:true
-        }
-      }]
-    }
-  }
-});
-*/
-
-/*
-$.ajax({
-  url: 'history.json',
-  dataType: 'json',
-  type: 'get',
-  cache: false,
-  success: function(data) {
-    $(data.services).each(function (index, value) {
-      console.log(value.name);
-    });
-  }
-});
-*/
